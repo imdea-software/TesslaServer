46214b6d633f56c67bb037c6ab7c187597e64531
Alexander Schramm <alexanderschramm@me.com>
2016-04-25 16:20:08 +0200

    Implements Stream type

--- lib/tessla_server/stream.ex
+++ lib/tessla_server/stream.ex
@@ -12,4 +12,26 @@ defmodule TesslaServer.Stream do
 
   @type t :: %__MODULE__{progressed_to: Timex.Types.timestamp, stream_name: atom, events: [Event.t]}
   defstruct progressed_to: Time.zero, stream_name: :none, events: []
+
+  @doc """
+  Progresses the `Stream` to the given `timestamp`
+  """
+  @spec progress(Stream.t, Timex.Types.timestamp) :: Stream.t
+  def progress(stream, timestamp) do
+    %{stream | progressed_to: timestamp}
+  end
+
+  @doc """
+  Adds the `Event` to the `Stream`.
+  The `timestamp` of the `Event` has to be greater or equal to to `progressed_to` value
+  of the `Stream`.
+  This method will advance the `progressed_to` field to the `timestamp` of the `Event`.
+  """
+  @spec add_event(Stream.t, Event.t) :: Stream.t
+  def add_event(stream, event) do
+    %{stream |
+     events: [event | stream.events],
+     progressed_to: event.timestamp
+   }
+  end
 end
