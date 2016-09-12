c73f9ba2886f448d9472f2f609b3f144ac557a1c
Alexander Schramm <alexanderschramm@me.com>
2016-09-12 09:06:38 +0200

    Test Data for Num_cores for v1 and V1.5

---
+++ benchmark/num_cores/bench.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+num_cores=(1 2 4 8 16);
+
+for i in "${num_cores[@]}"
+do
+  echo dumbbench --table=${i}_cores.dat -p 0.0001 -m 5 -- ./tessla_server_$i parallel-16.tessla --trace traces.1 -o 4:finished
+done
