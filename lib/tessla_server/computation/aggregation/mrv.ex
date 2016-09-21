ea8616dd398ebb12b6e0c565b123e3bd8a1b4545
Alexander Schramm <alexanderschramm@me.com>
2016-09-21 16:27:14 +0200

    Implement MRV Computation

--- lib/tessla_server/computation/aggregation/mrv.ex
+++ lib/tessla_server/computation/aggregation/mrv.ex
@@ -2,35 +2,51 @@ defmodule TesslaServer.Computation.Aggregation.Mrv do
   @moduledoc """
   Implements a `Computation` that emits a Signal holding the value of the latest event happened on the input.
   To do so the `state.operands` list has to be initialized with one id representing the id of
-  the EventStream which values should be converted into a Signal.
+  the EventStream which values should be converted into a Signal and options has to hold
+  the initial value under the key `default`.
   """
 
-  alias TesslaServer.{GenComputation, Event}
+  alias TesslaServer.{GenComputation, Event, Registry}
   alias TesslaServer.Computation.State
 
   use GenComputation
-  use Timex
 
-  # def perform_computation(timestamp, event_map, state) do
-  #   last_event = History.latest_output state.history
-  #   new_event = event_map[hd(state.operands)]
-  #   changed = (last_event.value != new_event.value)
-  #   if changed do
-  #     {:ok, %Event{
-  #       stream_id: state.stream_id, timestamp: timestamp, value: new_event.value
-  #     }}
-  #   else
-  #     :wait
-  #   end
-  # end
+  def init(state) do
+    Registry.subscribe_to :source
+    super state
+  end
 
-  # def init_output(state) do
-  #   default_value = state.options[:default]
-  #   default_event = %Event{stream_id: state.stream_id, value: default_value}
+  def handle_cast(:start_evaluation, state) do
+    first_event = %Event{
+      stream_id: state.stream_id, value: state.cache[:last_value], type: output_event_type
+    }
 
-  #   {:ok, history} = History.update_output(state.history, default_event)
-  #   %{history.output | type: output_stream_type}
-  # end
+    Enum.each state.children, fn child ->
+      GenComputation.send_event child, first_event
+    end
+    {:noreply, state}
+  end
 
-  # def output_stream_type, do: :signal
+  def process_event_map(event_map, timestamp, state) do
+    new_event = event_map[hd(state.operands)]
+
+    case new_event.type do
+      :event ->
+        last_value = Map.get state.cache, :last_value
+        if new_event.value != last_value do
+          {:ok, %Event{
+            stream_id: state.stream_id, timestamp: timestamp, value: new_event.value, type: output_event_type
+          }, %{last_value: new_event.value}}
+        else
+          {:progress, state.cache}
+        end
+      :progress -> {:progress, state.cache}
+    end
+  end
+
+  def init_cache(state) do
+    %{last_value: state.options[:default]}
+  end
+
+  def output_event_type, do: :change
 end
--- test/tessla_server/computation/aggregation/mrv_test.exs
+++ test/tessla_server/computation/aggregation/mrv_test.exs
@@ -3,9 +3,8 @@ defmodule TesslaServer.Computation.Aggregation.MrvTest do
   use Timex
 
   alias TesslaServer.Computation.Aggregation.Mrv
-  alias TesslaServer.{Event, GenComputation, Registry}
+  alias TesslaServer.{Event, Source, GenComputation, Registry}
 
-  import DateTime, only: [now: 0, shift: 2, to_timestamp: 1]
   import System, only: [unique_integer: 0]
 
   doctest Mrv
@@ -23,37 +22,56 @@ defmodule TesslaServer.Computation.Aggregation.MrvTest do
 
   test "Should change value of signal on every event with new value" do
     GenComputation.add_child(@processor, @test)
-    assert_receive({_, {:update_input_stream, %{type: :signal, events: [out0]}}})
-    assert(out0.value == @default)
 
-    timestamp = DateTime.now
-    event1 = %Event{value: 3, timestamp: to_timestamp(timestamp), stream_id: @op1}
+    Source.start_evaluation
+    assert_receive({_, {:process, change0}})
+    assert change0.timestamp == Duration.zero
+    assert change0.value == @default
+    assert change0.type == :change
+
+    timestamp1 = Duration.now
+    timestamp2 = Duration.add(timestamp1, Duration.from_seconds(1))
+    timestamp3 = Duration.add(timestamp1, Duration.from_seconds(2))
+    timestamp4 = Duration.add(timestamp1, Duration.from_seconds(3))
+
+    event1 = %Event{timestamp: timestamp1, value: 6, stream_id: @op1}
     event2 = %Event{
-      value: 3, timestamp: to_timestamp(shift(timestamp, seconds: 2)), stream_id: @op1
+      timestamp: timestamp2, value: 4, stream_id: @op1
     }
     event3 = %Event{
-      value: 5, timestamp: to_timestamp(shift(timestamp, seconds: 3)), stream_id: @op1
+      timestamp: timestamp3, value: 4, stream_id: @op1
     }
+    event4 = %Event{
+      timestamp: timestamp4, stream_id: @op1, type: :progress
+    }
+
 
     GenComputation.send_event(@processor, event1)
 
-    assert_receive {_,
-      {:update_input_stream, %{progressed_to: progressed_to, events: [out1, ^out0]}}
-    }
-    assert progressed_to == event1.timestamp
-    assert out1.timestamp == event1.timestamp
-    assert out1.value == event1.value
+    assert_receive({_, {:process, change1}})
+    assert change1.timestamp == timestamp1
+    assert change1.type == :change
+    assert change1.value == 6
 
     GenComputation.send_event(@processor, event2)
-    assert_receive {_,
-      {:update_input_stream, %{progressed_to: progressed_to, events: [^out1, ^out0]}}
-    }
-    assert progressed_to == event2.timestamp
+
+    assert_receive({_, {:process, change2}})
+    assert change2.timestamp == timestamp2
+    assert change2.value == event2.value
+    assert change2.type == :change
 
     GenComputation.send_event(@processor, event3)
-    assert_receive({_, {:update_input_stream, %{events: [out2, ^out1, ^out0]}}})
-    assert out2.value == event3.value
-    assert out2.timestamp == event3.timestamp
+
+    assert_receive({_, {:process, change3}})
+    assert change3.timestamp == timestamp3
+    assert change3.type == :progress
+
+    GenComputation.send_event(@processor, event4)
+
+    assert_receive({_, {:process, change4}})
+    assert change4.timestamp == timestamp4
+    assert change3.type == :progress
+
 
     :ok = GenComputation.stop(@processor)
   end
