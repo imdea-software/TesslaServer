73d97eead7af3541cc02dc62b9eecdaaeec4a8ac
Alexander Schramm <alexanderschramm@me.com>
2016-10-03 17:02:57 +0200

    Implemented DelaySignalByTime Computation.

--- test/tessla_server/computation/timing/delay_signal_by_time_test.exs
+++ test/tessla_server/computation/timing/delay_signal_by_time_test.exs
@@ -5,73 +5,83 @@ defmodule TesslaServer.Computation.Timing.DelaySignalByTimeTest do
   alias TesslaServer.Computation.Timing.DelaySignalByTime
   alias TesslaServer.{Event, GenComputation, Registry}
 
-  import DateTime, only: [now: 0, shift: 2, to_timestamp: 1]
   import System, only: [unique_integer: 0]
 
   doctest DelaySignalByTime
 
-  @op1 unique_integer
+  @op unique_integer
   @amount 1000
-  @default 0
+  @default -1
   @test unique_integer
   @processor unique_integer
 
   setup do
     Registry.register @test
-    DelaySignalByTime.start @processor, [@op1], %{amount: @amount, default: @default}
+    DelaySignalByTime.start @processor, [@op], %{amount: @amount, default: @default}
     :ok
   end
 
-  test "Should delay events by specified amount" do
+  test "Should delay changes by specified amount" do
     GenComputation.add_child(@processor, @test)
 
-    assert_receive({_, {:update_input_stream, %{type: :signal, events: [out0]}}})
-    assert out0.value == @default
+    shift_duration = Duration.from_microseconds @amount
 
-    event1 = %Event{timestamp: {0, 1, 0}, stream_id: @op1, value: 1}
-    event2 = %Event{timestamp: {0, 1, 10}, stream_id: @op1, value: 2}
-    event3 = %Event{timestamp: {0, 1, 20}, stream_id: @op1, value: 3}
-    event4 = %Event{timestamp: {0, 1, 1010}, stream_id: @op1, value: 4}
-    event5 = %Event{timestamp: {0, 5, 0}, stream_id: @op1, value: 5}
+    timestamp0 = Duration.zero
+    timestamp1 = Duration.now
+    timestamp2 = Duration.add(timestamp1, Duration.from_seconds(1))
+    timestamp3 = Duration.add(timestamp1, Duration.from_seconds(2))
+    timestamp4 = Duration.add(timestamp1, Duration.from_seconds(3))
 
+    change0 = %Event{timestamp: timestamp0, stream_id: @op, value: @default, type: :change}
+    change1 = %Event{timestamp: timestamp1, stream_id: @op, value: 1, type: :change}
+    change2 = %Event{
+      timestamp: timestamp2, stream_id: @op, value: 2, type: :change
+    }
+    change3 = %Event{
+      timestamp: timestamp3, stream_id: @op, type: :progress
+    }
+    change4 = %Event{
+      timestamp: timestamp4, stream_id: @op, value: 4, type: :change
+    }
 
-    GenComputation.send_event(@processor, event1)
+    GenComputation.send_event(@processor, change0)
 
-    assert_receive({_, {:update_input_stream, %{progressed_to: progressed_to, events: [^out0]}}})
-    assert progressed_to == event1.timestamp
+    shifted_timestamp0 = Duration.add timestamp0, shift_duration
+    assert_receive {_, {:process,
+      %Event{type: :change, timestamp: ^timestamp0, value: @default}}
+    }
+    assert_receive {_, {:process,
+      %Event{type: :progress, timestamp: ^shifted_timestamp0}}
+    }
 
-    GenComputation.send_event(@processor, event2)
+    GenComputation.send_event(@processor, change1)
 
-    assert_receive({_, {:update_input_stream, %{progressed_to: progressed_to, events: [^out0]}}})
-    assert progressed_to == event2.timestamp
+    shifted_timestamp1 = Duration.add timestamp1, shift_duration
+    assert_receive {_, {:process,
+      %Event{type: :change, timestamp: ^shifted_timestamp1, value: 1}}
+    }
 
-    GenComputation.send_event(@processor, event3)
-    assert_receive({_, {:update_input_stream, %{progressed_to: progressed_to, events: [^out0]}}})
-    assert progressed_to == event3.timestamp
+    GenComputation.send_event(@processor, change2)
 
-    GenComputation.send_event(@processor, event4)
-    assert_receive({_, {:update_input_stream, %{progressed_to: progressed_to, events: [out2, out1, ^out0]}}})
-    assert is_delayed(out1.timestamp, event1.timestamp)
-    assert out1.value == event1.value
-    assert is_delayed(out2.timestamp, event2.timestamp)
-    assert out2.value == event2.value
-    assert progressed_to == event4.timestamp
+    shifted_timestamp2 = Duration.add timestamp2, shift_duration
+    assert_receive {_, {:process,
+      %Event{type: :change, timestamp: ^shifted_timestamp2, value: 2}}
+    }
 
-    GenComputation.send_event(@processor, event5)
+    GenComputation.send_event(@processor, change3)
 
-    assert_receive({_, {:update_input_stream, %{progressed_to: progressed_to,
-        events: [out4, out3, ^out2, ^out1, ^out0]}}})
-    assert is_delayed(out3.timestamp, event3.timestamp)
-    assert out3.value == event3.value
-    assert is_delayed(out4.timestamp, event4.timestamp)
-    assert out4.value == event4.value
-    assert progressed_to == event5.timestamp
+    shifted_timestamp3 = Duration.add timestamp3, shift_duration
+    assert_receive {_, {:process,
+      %Event{type: :progress, timestamp: ^shifted_timestamp3}}
+    }
+
+    GenComputation.send_event(@processor, change4)
+
+    shifted_timestamp4 = Duration.add timestamp4, shift_duration
+    assert_receive {_, {:process,
+      %Event{type: :change, timestamp: ^shifted_timestamp4, value: 4}}
+    }
 
     :ok = GenComputation.stop(@processor)
-  end
-
-  @spec is_delayed(Timex.Types.timestamp, Timex.Types.timestamp) :: boolean
-  defp is_delayed(time1, time2) do
-    Time.diff(time1, time2) == Time.from(@amount, :microseconds)
   end
 end
