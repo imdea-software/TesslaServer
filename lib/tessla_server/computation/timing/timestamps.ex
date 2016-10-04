f60e97e5ad431ff83f50bd9e3e2e319d66dfc851
Alexander Schramm <alexanderschramm@me.com>
2016-10-04 09:30:25 +0200

    Implemented Timestamps Computation.
    End of refactoring.

--- lib/tessla_server/computation/timing/timestamps.ex
+++ lib/tessla_server/computation/timing/timestamps.ex
@@ -1,7 +1,7 @@
 defmodule TesslaServer.Computation.Timing.Timestamps do
   @moduledoc """
   Implements a `Computation` that emits an Event everytime an input Event is
-  received and has it's timestamp as it's value.
+  received and has its timestamp as it's value.
   To do so the `state.operands` list has to be initialized with one id representing
   the id of the Event Stream which timestamps should be emitted.
   """
@@ -9,11 +9,20 @@ defmodule TesslaServer.Computation.Timing.Timestamps do
   alias TesslaServer.{GenComputation, Event}
 
   use GenComputation
+  use Timex
 
-  # def perform_computation(timestamp, event_map, state) do
-  #   new_event = event_map[hd(state.operands)]
-  #   {:ok, %Event{
-  #     stream_id: state.stream_id, timestamp: timestamp, value: new_event.timestamp
-  #   }}
-  # end
+  def process_event_map(event_map, timestamp, state) do
+    new_event = event_map[hd(state.operands)]
+    case new_event.type do
+      :event ->
+        output_event = %Event{
+          timestamp: timestamp, stream_id: state.stream_id,
+          value: timestamp
+        }
+        {:ok, output_event, %{}}
+      :progress ->
+        {:progress, %{}}
+    end
+  end
+
 end
--- test/tessla_server/computation/timing/timestamps_test.exs
+++ test/tessla_server/computation/timing/timestamps_test.exs
@@ -5,53 +5,52 @@ defmodule TesslaServer.Computation.Timing.TimestampsTest do
   alias TesslaServer.Computation.Timing.Timestamps
   alias TesslaServer.{Event, GenComputation, Registry}
 
-  import DateTime, only: [now: 0, shift: 2, to_timestamp: 1]
   import System, only: [unique_integer: 0]
 
   doctest Timestamps
 
-  @op1 unique_integer
+  @op unique_integer
   @test unique_integer
   @processor unique_integer
 
   setup do
     Registry.register @test
-    Timestamps.start @processor, [@op1]
+    Timestamps.start @processor, [@op]
     :ok
   end
 
   test "should emit timestamp of every received event" do
     GenComputation.add_child(@processor, @test)
-    assert_receive({_, {:update_input_stream, %{type: :events, events: []}}})
 
-    timestamp1 = DateTime.now
-    timestamp2 = shift(timestamp1, seconds: 2)
-    timestamp3 = shift(timestamp1, seconds: 3)
-    event1 = %Event{timestamp: to_timestamp(timestamp1), stream_id: @op1}
+    timestamp1 = Duration.now
+    timestamp2 = Duration.add(timestamp1, Duration.from_seconds(1))
+    timestamp3 = Duration.add(timestamp1, Duration.from_seconds(2))
+
+    event1 = %Event{timestamp: timestamp1, stream_id: @op, value: 1}
     event2 = %Event{
-      timestamp: to_timestamp(timestamp2), stream_id: @op1
+      timestamp: timestamp2, stream_id: @op, type: :progress
     }
     event3 = %Event{
-      timestamp: to_timestamp(timestamp3), stream_id: @op1
+      timestamp: timestamp3, stream_id: @op, value: 4
     }
 
     GenComputation.send_event(@processor, event1)
 
-    assert_receive({_, {:update_input_stream, %{events: [out0]}}})
-    assert out0.timestamp == event1.timestamp
-    assert out0.value == event1.timestamp
+    assert_receive {_, {:process,
+      %Event{type: :event, timestamp: ^timestamp1, value: ^timestamp1}}
+    }
 
     GenComputation.send_event(@processor, event2)
 
-    assert_receive({_, {:update_input_stream, %{events: [out1, ^out0]}}})
-    assert out1.value == event2.timestamp
-    assert out1.timestamp == event2.timestamp
+    assert_receive {_, {:process,
+      %Event{timestamp: ^timestamp2, type: :progress}}
+    }
 
     GenComputation.send_event(@processor, event3)
 
-    assert_receive({_, {:update_input_stream, %{events: [out2, ^out1, ^out0]}}})
-    assert out2.value == event3.timestamp
-    assert out2.timestamp == event3.timestamp
+    assert_receive {_, {:process,
+      %Event{type: :event, timestamp: ^timestamp3, value: ^timestamp3}}
+    }
 
     :ok = GenComputation.stop(@processor)
   end
